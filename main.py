40ec3089c5abc0e11f6ae450635166ce25ddc81e
CEDZEE <159541365+cedzeedev@users.noreply.github.com>
2024-02-13 10:37:19 +0100

    Published Uncrypter V3
    I created and published for you: Uncrypter V3

---
+++ main.py
@@ -0,0 +1,21 @@
+print("v3.0 | By CEDZEE")
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for x in range(0,100):
+    print("Entrez un message à crypter")
+    chaîneACrypter = input("")
+    chaîneACrypter = chaîneACrypter.upper()
+    quantitéDécalage = int(input("Entre un nombre entre 1 et 25 pour crypter/décrypter ton message"))
+    chaîneCryptée = ""
+    for caractèreActuel in chaîneACrypter:
+        position = alphabet.find(caractèreActuel)
+        nouvellePosition = position + quantitéDécalage
+        if caractèreActuel in alphabet:
+            chaîneCryptée = chaîneCryptée + alphabet[nouvellePosition]
+        else:
+            chaîneCryptée = chaîneCryptée + caractèreActuel
+    for x in range(0, 2000):
+        print("-----------------------")
+    print("Message:", chaîneCryptée)
+    print("Thanks for use my programm")
+
